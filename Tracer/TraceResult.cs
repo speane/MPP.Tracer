2ecfa27011cb6084daa2881c053549d736d6d83d
Lenny <roman.alshevscky@gmail.com>
2016-09-13 10:08:36 +0300

    Add tracer library

---
+++ Tracer/TraceResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public class TraceResult
+    {
+        private string className;
+        private string methodName;
+        private long interval;
+        private int argCount;
+
+        public TraceResult(string className, string methodName, long interval, int argCount)
+        {
+            this.className = className;
+            this.methodName = methodName;
+            this.interval = interval;
+            this.argCount = argCount;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2} {3}", className, methodName, interval, argCount);
+        }
+    }
+}
---
+++ Tracer/Tracer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace Tracer
+{
+    public class Tracer : ITracer
+    {
+        Stopwatch timer = new Stopwatch();
+
+        public void StartTrace()
+        {
+            
+            timer.Start();
+        }
+
+        public void StopTrace()
+        {
+            timer.Stop();
+
+        }
+
+        public TraceResult GetResult()
+        {
+            StackTrace st = new StackTrace();
+            StackFrame sf = st.GetFrame(1);
+            string name = sf.GetMethod().Name;
+            string className = sf.GetMethod().DeclaringType.Name;
+            int argCount = sf.GetMethod().GetParameters().Length;
+            long time = timer.ElapsedMilliseconds;
+            return new TraceResult(className, name, time, argCount);
+        }
+
+    }
+}
