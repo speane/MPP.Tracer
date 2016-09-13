efccb78ee9c987adc48f38f3fc9165bce6dbd6e9
Lenny <roman.alshevscky@gmail.com>
2016-09-13 10:08:01 +0300

    Add testing application

---
+++ Tester/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracer;
+
+namespace Tester
+{
+    class Program
+    {
+        static Tracer.Tracer Tracer = new Tracer.Tracer();
+        static void Main(string[] args)
+        {
+            Tracer.StartTrace();
+            RunTest(20000000);
+            Tracer.StopTrace();
+            Console.WriteLine(Tracer.GetResult().ToString());
+        }
+
+        private static void RunTest(int cycles)
+        {
+            Tracer.Tracer Tracer = new Tracer.Tracer();
+            Tracer.StartTrace();
+            for (int i = 0; i < cycles; i++)
+            {
+                int a = cycles * cycles;
+            }
+            Tracer.StopTrace();
+            Console.WriteLine(Tracer.GetResult().ToString());
+        }
+    }
+}
