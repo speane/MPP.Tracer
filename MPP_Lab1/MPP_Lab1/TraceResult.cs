f4ba9ad8101f5db84acea9d945e2c50dd96bfc70
Sergey <sergeybirich@gmail.com>
2016-09-18 20:34:28 +0300

    implemented class TraceResult

--- MPP_Lab1/MPP_Lab1/TraceResult.cs
+++ MPP_Lab1/MPP_Lab1/TraceResult.cs
@@ -8,5 +8,24 @@ namespace MPP_Lab1
 {
     class TraceResult
     {
+        public List<TraceResult> childs;
+        private string methodName;
+        private string className;
+        private int executionTime;
+        private int parametrsNumber;
+        private int threadId;
+        public string MethodName { get { return methodName; } set { methodName = value ; } }
+        public string ClassName { get { return className; } set { className = value; } }
+        public int ExecutionTime { get { return executionTime; } set { executionTime = value; } }
+        public int ParametrsNumber { get { return parametrsNumber; } set { parametrsNumber = value; } }
+        public int ThreadId { get { return threadId; } set { threadId = value; } }
+        public TraceResult (string name, string className, int time, int number, int id)
+        {
+            methodName = name;
+            this.className = className;
+            executionTime = time;
+            parametrsNumber = number;
+            threadId = id;
+        }
     }
 }
