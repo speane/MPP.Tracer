f1a0c9f288ed2d27bdf264b594793a7604f95655
Sergey <sergeybirich@gmail.com>
2016-09-19 17:59:04 +0300

    Added ITraceResultFormatter and implemented this interface for console

---
+++ MPP_Lab1/MPP_Lab1/ITraceResultFormatter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPP_Lab1
+{
+    interface ITraceResultFormatter
+    {
+        void Format(TraceResult result);
+    }
+}
