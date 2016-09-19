3b22fd1d4dcf83994e4a5ec5954877a0cfc97814
Sergey <sergeybirich@gmail.com>
2016-09-19 16:16:32 +0300

    Added methods StartWatch and StopWatch to TraceResult

--- MPP_Lab1/MPP_Lab1/TraceResult.cs
+++ MPP_Lab1/MPP_Lab1/TraceResult.cs
@@ -12,17 +12,26 @@ namespace MPP_Lab1
         public List<TraceResult> childs;
         private string methodName;
         private string className;
-        private int executionTime;
+        private long executionTime;
         private int parametrsNumber;
         private int threadId;
         private bool isThreadChild;
         private Stopwatch watch;
         public string MethodName { get { return methodName; } set { methodName = value ; } }
         public string ClassName { get { return className; } set { className = value; } }
-        public int ExecutionTime { get { return executionTime; } set { executionTime = value; } }
+        public long ExecutionTime { get { return executionTime; } set { executionTime = value; } }
         public int ParametrsNumber { get { return parametrsNumber; } set { parametrsNumber = value; } }
         public bool IsThreadChild { get { return isThreadChild; } }
         public int ThreadId { get { return threadId; } set { threadId = value; } }
+        public void StartWatch()
+        {
+            watch.Start();
+        }
+        public void StopWatch()
+        {
+            watch.Stop();
+            executionTime = watch.ElapsedMilliseconds;
+        }
         public TraceResult (string name, string className, int time, int number)
         {
             methodName = name;
@@ -33,7 +42,6 @@ namespace MPP_Lab1
             isThreadChild = false;
             childs = new List<TraceResult>();
             watch = new Stopwatch();
-            watch.Start();
         }
         public TraceResult(int id)
         {
@@ -41,7 +49,6 @@ namespace MPP_Lab1
             isThreadChild = true;
             childs = new List<TraceResult>();
             watch = new Stopwatch();
-            watch.Start();
         }
     }
 }
