7b4213607646b4d80a05e697a854d343e29870fd
zasamen <kardashvg@gmail.com>
2016-09-22 08:59:30 +0300

    Fixed constructor

--- Tracer.cs
+++ Tracer.cs
@@ -16,6 +16,11 @@ namespace MPP.Tracer
             }
         }
        
+        static Tracer()
+        {
+
+        }
+
         private Tracer()
         {
             traceResult = new TraceResult();
